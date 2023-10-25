2bc95112749ead6f448f93c4d8fdcf91bdffe025
Michał Charewicz <michal.charewicz@gmail.com>
2023-10-25 20:59:42 +0200

    kodilla-intro

---
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -0,0 +1,6 @@
+public class FirstClass {                           // [1]
+    public static void main(String[] args) {         // [2]
+        String hello = "Hello from FirstClass!";
+        System.out.println(hello); // [3]
+    }
+}
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,12 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String carBrand = "Kia";
+        String name = "Stonic";
+        String color = "Yelow";
+        int horsePower = 120;
+        boolean fourWhealDrive = false;
+        char drivingLicence = 'B';
+
+        System.out.println(drivingLicence);
+    }
+}
