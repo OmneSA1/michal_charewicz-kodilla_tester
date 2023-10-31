c096692239dccfb7ef7be19321f92e01e7c892a1
Michał Charewicz <michal.charewicz@gmail.com>
2023-10-31 19:23:42 +0100

    Loops

---
+++ kodilla-intro/src/main/java/Loops.java
@@ -0,0 +1,16 @@
+public class Loops {
+    public static void main(String[] args) {
+        String[] names = new String[] {"Zygfryd", "Gwidon", "Florentyna"};
+        int numberOfLength = names.length;
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+
+//Wyświetlanie elementów tablicy od końca //////
+        for (int i = names.length - 1; i >= 0; i--) {
+            System.out.println(names[i]);
+        }
+
+
+}
+}
---
+++ kodilla-intro/src/main/java/SimpleAray.java
@@ -0,0 +1,12 @@
+public class SimpleAray {
+    public static void main(String[] args) {
+    String[] friends = new String[] {"Maciej", "Marcin", "Magda", "Robert", "Kamil"};
+
+    String friend = friends [3];
+    System.out.println(friend);
+
+    int numberOfElements = friends.length;
+
+    System.out.println("Moja tablica zawiera " + numberOfElements + " elementów." );
+    }
+}
