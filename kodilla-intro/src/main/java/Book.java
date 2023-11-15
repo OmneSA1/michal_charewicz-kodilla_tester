9cf328eb9bf5e534cd397e67908bfdfb2d996308
Michał Charewicz <michal.charewicz@gmail.com>
2023-11-15 21:10:44 +0100

    Poprawka do zadania o metodach statycznych  Book

--- kodilla-intro/src/main/java/Book.java
+++ kodilla-intro/src/main/java/Book.java
@@ -1,10 +1,17 @@
 public class Book {
+    public static void main(String[] args) {
+        Book book = Book.of("Isaac Asimov", "The Galaxy");
+    }
     private String author;
     private String title;
 
-    private Book(String author, String title){
+    private Book(String author, String title) {
         this.author = author;
-        this.title  =title;
+        this.title = title;
     }
 
+
+    public static Book of(String author, String title) {
+        return new Book(author, title);
+    }
 }
