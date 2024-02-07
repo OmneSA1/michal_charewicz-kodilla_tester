08dd56324f9083e14fabfc26172e2a02204ef6bb
Michał Charewicz <michal.charewicz@gmail.com>
2024-02-07 07:09:53 +0100

    Zadanie: testy z REST Assured

---
+++ kodilla-rest-api/src/test/java/com/kodilla/rest/controller/ExternalApiRestAssuredTest.java
@@ -0,0 +1,44 @@
+package com.kodilla.rest.controller;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+public class ExternalApiRestAssuredTest {
+    @Test
+    void testGetPostShouldReturnStatus200AndValidateResponse() {
+        given()
+                .header("Content-Type", "application/json")
+                .when()
+                .get("https://jsonplaceholder.typicode.com/posts/1")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body("userId", equalTo(1))
+                .body("title", equalTo("sunt aut facere repellat provident occaecati excepturi optio reprehenderit"))
+                .body("body", equalTo("quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto")).
+                log().all();
+    }
+    @Test
+    void testUpdatePostShouldReturnUpdatedResponse() {
+
+        Map<String, Object> updatedPostData = new HashMap<>();
+        updatedPostData.put("id", 1);
+        updatedPostData.put("title", "Updated title");
+
+        given()
+                .header("Content-Type", "application/json")
+                .body(updatedPostData)
+                .when()
+                .put("https://jsonplaceholder.typicode.com/posts/1")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body("id", equalTo(1))
+                .body("title", equalTo("Updated title"))
+                .log().all();
+    }
+}
